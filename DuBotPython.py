d55f37f1c5fb28e806f1c12b6e314b44440cce38
Duzo <73184526+Duzos@users.noreply.github.com>
2021-09-08 00:22:33 +0100

    updated the info command
    added what its based from

--- DuBotPython.py
+++ DuBotPython.py
@@ -131,8 +131,10 @@ def ball():
     print(f"Answer: {answer}")
 
 def info():
+    print("Based off of DuBot (https://discord.com/api/oauth2/authorize?client_id=865190020179296267&permissions=93184&scope=bot)")
     print("Created in Python")
     print("Created by James Hall (Duzo)")
+
 
 def help():
     print("Help:")
